79a4df0480d1dbac624815e4ac4cf87034d7f11c
yuik23 <yui_karamatsu@yahoo.co.jp>
2024-03-22 18:53:30 +0900

    プロジェクトのセットアップ

---
+++ src/main/java/com/membershipmanagement/memberservice/MemberserviceApplication.java
@@ -0,0 +1,13 @@
+package com.membershipmanagement.memberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberserviceApplication.class, args);
+	}
+
+}
